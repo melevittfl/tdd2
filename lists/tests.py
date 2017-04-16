617a7d89a668e5d73f89d83d28975e29a6e6a0c5
Mark Levitt <mark@marklevitt.co.uk>
2017-04-16 19:09:40 +0100

    About to refactor to handle multiple list items

--- lists/tests.py
+++ lists/tests.py
@@ -2,9 +2,15 @@ from django.test import TestCase
 
 # Create your tests here.
 
-class HomePageTest(TestCase):
 
+class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_post_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
