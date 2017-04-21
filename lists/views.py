b497802b939359ae1ec88c0f83a0a304dc04683b
Mark Levitt <mark@marklevitt.co.uk>
2017-04-21 12:39:10 +0100

    pg 113 - Changed the db to have a List

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html')
+
 
 def view_list(request):
     items = Item.objects.all()
@@ -12,5 +13,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
