8fa11f66d2ece29f8ff626fba9e32bca353a6379
Mark Levitt <mark@marklevitt.co.uk>
2017-04-23 12:11:59 +0100

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -105,9 +105,9 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list
-        page_text = self.browser.find_elements_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertin('Buy milk', page_text)
+        self.assertIn('Buy milk', page_text)
 
         self.fail('Finish the test!')
 
