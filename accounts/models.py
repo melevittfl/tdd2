d0e2aca9ab9ee6f9b708d3ab933e6c267771f34e
Mark Levitt <mark@marklevitt.co.uk>
2017-05-09 08:51:11 +0100

    Custom passwordless auth backend + custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
+from django.contrib import auth
 from django.db import models
+import uuid
+
+
+auth.signals.user_logged_in.disconnect(auth.models.update_last_login)
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    is_anonymous = False
+    is_authenticated = True
+
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(max_length=40, default=uuid.uuid4)
--- superlists/settings.py
+++ superlists/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
+]
+
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +142,8 @@ LOGGING = {
 },
     'root': {'level': 'INFO'},
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ruscombe25@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
